73045c3b4af6bdc883d1af77434d9e1e30d88c92
Taylor Arnett <tarnett1@mac.com>
2020-05-09 21:46:54 -0600

    first commit

---
+++ pre-sign/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
+	"os"
+	"time"
+)
+
+type FileMetaData struct {
+	Filename string `json:"file_name"`
+}
+
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess := session.Must(session.NewSession())
+
+	key, err := parseResponseStringToTypedObject(request.Body)
+	log.Println(key)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	req, _ := svc.PutObjectRequest(&s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("INPUT_BUCKET")),
+		Key: aws.String(key),
+	})
+	url, err := req.Presign(2 * time.Minute)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	return events.APIGatewayProxyResponse{Body: url, StatusCode: 200}, err
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+func parseResponseStringToTypedObject(responseString string) (string, error) {
+
+	b := []byte(responseString)
+	var resp FileMetaData
+	err := json.Unmarshal(b, &resp)
+
+	if err == nil {
+		return resp.Filename, nil
+	} else {
+		log.Print(fmt.Sprintf("Could not unmarshall JSON string: [%s]", err.Error()))
+		return err.Error(), err
+	}
+}
